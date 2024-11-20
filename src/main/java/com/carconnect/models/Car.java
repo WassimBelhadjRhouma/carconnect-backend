ac9ecbfa1e34f0d5c40ca4251ef60c1b1085dfae
Alaaeddine Hemdane <hemdane@myntag.onmicrosoft.com>
2024-11-20 13:21:20 +0100

    getters and setters for carconnect/models/Car.java

--- src/main/java/com/carconnect/models/Car.java
+++ src/main/java/com/carconnect/models/Car.java
@@ -36,5 +36,115 @@ public class Car {
     @OneToOne(mappedBy = "car", cascade = CascadeType.ALL)
     private ReportCheckAfterRenting reportCheckAfter;
 
-    // Getters, setters, constructors...
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getMake() {
+        return make;
+    }
+
+    public void setMake(String make) {
+        this.make = make;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public String getLicencePlate() {
+        return licencePlate;
+    }
+
+    public void setLicencePlate(String licencePlate) {
+        this.licencePlate = licencePlate;
+    }
+
+    public int getMileage() {
+        return mileage;
+    }
+
+    public void setMileage(int mileage) {
+        this.mileage = mileage;
+    }
+
+    public double getPricePerDay() {
+        return pricePerDay;
+    }
+
+    public void setPricePerDay(double pricePerDay) {
+        this.pricePerDay = pricePerDay;
+    }
+
+    public String getAvailability() {
+        return availability;
+    }
+
+    public void setAvailability(String availability) {
+        this.availability = availability;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public User getOwner() {
+        return owner;
+    }
+
+    public void setOwner(User owner) {
+        this.owner = owner;
+    }
+
+    public Set<Booking> getBookings() {
+        return bookings;
+    }
+
+    public void setBookings(Set<Booking> bookings) {
+        this.bookings = bookings;
+    }
+
+    public Set<Review> getReviews() {
+        return reviews;
+    }
+
+    public void setReviews(Set<Review> reviews) {
+        this.reviews = reviews;
+    }
+
+    public ReportCheckBeforeRenting getReportCheckBefore() {
+        return reportCheckBefore;
+    }
+
+    public void setReportCheckBefore(ReportCheckBeforeRenting reportCheckBefore) {
+        this.reportCheckBefore = reportCheckBefore;
+    }
+
+    public ReportCheckAfterRenting getReportCheckAfter() {
+        return reportCheckAfter;
+    }
+
+    public void setReportCheckAfter(ReportCheckAfterRenting reportCheckAfter) {
+        this.reportCheckAfter = reportCheckAfter;
+    }
 }
